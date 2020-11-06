1df3bf0f3fdf6e802b6555d7275a3b0a994363f3
Vivica <Ma>
2020-11-06 01:16:56 -0500

    use yarn instead of npm

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ function App() {
     <div className="App">
       <h1>Welcome to one-step supermarket!</h1>
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQo2rEpJXcNgac92KTGNzf5a2M_7SG_Oe8kew&usqp=CAU"/>
-      <p>testing</p>
+
+
+      <h3>🔥创建一个新的超市？</h3>
+      <button>👋点击这里</button>
     </div>
   );
 }
