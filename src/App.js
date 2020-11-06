7fb63314257a9ce6243a0f054cdb85e3cd6af83f
Vivica <Ma>
2020-11-06 00:47:35 -0500

    testing

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to one-step supermarket!</h1>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQo2rEpJXcNgac92KTGNzf5a2M_7SG_Oe8kew&usqp=CAU"/>
     </div>
   );
 }
